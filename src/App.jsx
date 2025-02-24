78184c73c6670846c9d4839cc4d1ca9f04f4e75c
Blank-ally <lia.stith@gmail.com>
2025-02-24 17:09:45 -0600

    int

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,45 @@
 import { useState, useRef } from "react";
 import Country from "./components/Country";
+import {
+  Theme,
+  Button,
+  Flex,
+  Heading,
+  Badge,
+  Container,
+  Grid,
+} from "@radix-ui/themes";
+import { SunIcon, MoonIcon } from "@radix-ui/react-icons";
+import "@radix-ui/themes/styles.css";
 import "./App.css";
 import NewCountry from "./components/NewCountry";
 
 function App() {
+  const [appearance, setAppearance] = useState("dark");
   const [countries, setCountries] = useState([
     { id: 1, name: "United States", gold: 2, silver: 2, bronze: 3 },
     { id: 2, name: "China", gold: 3, silver: 1, bronze: 0 },
     { id: 3, name: "France", gold: 0, silver: 2, bronze: 2 },
+    { id: 4, name: "Germany", gold: 0, silver: 2, bronze: 2 },
+    { id: 5, name: "Spain", gold: 1, silver: 1, bronze: 0 },
+    { id: 6, name: "United Kingdom", gold: 0, silver: 2, bronze: 3 },
+    { id: 7, name: "Brazil", gold: 3, silver: 0, bronze: 0 },
+    { id: 8, name: "Italy", gold: 2, silver: 2, bronze: 2 },
+    { id: 9, name: "Switzerland", gold: 1, silver: 1, bronze: 2 },
+    { id: 10, name: "Poland", gold: 0, silver: 2, bronze: 1 },
+    { id: 11, name: "Sweden", gold: 0, silver: 3, bronze: 1 },
+    { id: 12, name: "Ireland", gold: 2, silver: 1, bronze: 0 },
+    { id: 13, name: "Scotland", gold: 3, silver: 0, bronze: 2 },
   ]);
   const medals = useRef([
-    { id: 1, name: "gold" },
-    { id: 2, name: "silver" },
-    { id: 3, name: "bronze" },
+    { id: 1, name: "gold", color: "#FFD700" },
+    { id: 2, name: "silver", color: "#C0C0C0" },
+    { id: 3, name: "bronze", color: "#CD7F32" },
   ]);
 
+  function toggleAppearance() {
+    setAppearance(appearance === "light" ? "dark" : "light");
+  }
   function handleAdd(name) {
     console.log(`add country: ${name}`);
     setCountries(
@@ -55,22 +80,39 @@ function App() {
   }
 
   return (
-    <>
-      <h1 id="page-header">Olympic Medals {getAllMedalsTotal()}</h1>
-      <div className="countries">
-        {countries.map((country) => (
-          <Country
-            onDelete={handleDelete}
-            onIncrement={handleIncrement}
-            onDecrement={handleDecrement}
-            key={country.id}
-            country={country}
-            medals={medals.current}
-          />
-        ))}
-      </div>
-      <NewCountry onAdd={handleAdd} />
-    </>
+    <Theme appearance={appearance}>
+      <Button
+        onClick={toggleAppearance}
+        style={{ position: "fixed", bottom: 20, right: 20, zIndex: 100 }}
+        variant="ghost"
+      >
+        {appearance === "dark" ? <MoonIcon /> : <SunIcon />}
+      </Button>
+      <Flex p="2" pl="8" className="fixedHeader" justify="between">
+        <Heading size="6">
+          Olympic Medals
+          <Badge variant="outline" ml="2">
+            <Heading size="6">{getAllMedalsTotal()}</Heading>
+          </Badge>
+        </Heading>
+        <NewCountry onAdd={handleAdd} />
+      </Flex>
+      <Container className="bg"></Container>
+      <Grid pt="2" gap="2" className="grid-container">
+        {countries
+          .sort((a, b) => a.name.localeCompare(b.name))
+          .map((country) => (
+            <Country
+              key={country.id}
+              country={country}
+              medals={medals.current}
+              onDelete={handleDelete}
+              onIncrement={handleIncrement}
+              onDecrement={handleDecrement}
+            />
+          ))}
+      </Grid>
+    </Theme>
   );
 }
 
